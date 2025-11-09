e986c627fe34e4cc9a9b1b8921f37f84e2b3ec73
Serhii Volskyi <serhii.volskyi@radity.com>
2025-11-09 02:33:51 +0200

    update `useMediaQuery` hook & add `useIsomorphicLayoutEffect` hook

--- hooks/useMediaQuery.ts
+++ hooks/useMediaQuery.ts
@@ -1,21 +1,75 @@
-import { useEffect, useState } from 'react';
+"use client"
+import { useState } from "react"
+import useIsomorphicLayoutEffect from "./useIsomorphicLayoutEffect"
 
-const useMediaQuery = (direction: 'above' | 'below', screenSize: number) => {
-  const [isMobile, setIsMobile] = useState(false);
+type UseMediaQueryOptions = {
+  defaultValue?: boolean
+  initializeWithValue?: boolean
+}
 
-  useEffect(() => {
-    const checkMobile = () => {
-      setIsMobile(
-        direction === 'above' ? window.innerWidth >= screenSize : window.innerWidth < screenSize
-      );
-    };
+type MediaQueryDirection = 'above' | 'below'
 
-    checkMobile();
-    window.addEventListener('resize', checkMobile);
-    return () => window.removeEventListener('resize', checkMobile);
-  }, [direction, screenSize]);
+const IS_SERVER = typeof window === "undefined"
 
-  return isMobile;
-};
+// Convert direction + screenSize to CSS media query
+const buildMediaQuery = (direction: MediaQueryDirection, screenSize: number): string => {
+  return direction === 'above'
+    ? `(min-width: ${screenSize}px)`
+    : `(max-width: ${screenSize - 1}px)`
+}
 
-export default useMediaQuery;
+export default function useMediaQuery(
+  direction: MediaQueryDirection,
+  screenSize: number,
+  {
+    defaultValue = false,
+    initializeWithValue = true,
+  }: UseMediaQueryOptions = {},
+): boolean {
+  const query = buildMediaQuery(direction, screenSize)
+
+  const getMatches = (query: string): boolean => {
+    if (IS_SERVER) {
+      return defaultValue
+    }
+    return window.matchMedia(query).matches
+  }
+
+  const [matches, setMatches] = useState<boolean>(() => {
+    if (initializeWithValue) {
+      return getMatches(query)
+    }
+    return defaultValue
+  })
+
+  console.log({matches, screenSize});
+
+  // Handles the change event of the media query.
+  function handleChange() {
+    setMatches(getMatches(query))
+  }
+
+  useIsomorphicLayoutEffect(() => {
+    const matchMedia = window.matchMedia(query)
+    // Triggered at the first client-side load and if query changes
+    handleChange()
+    // Use deprecated `addListener` and `removeListener` to support Safari < 14 (#135)
+    if (matchMedia.addListener) {
+      matchMedia.addListener(handleChange)
+    } else {
+      matchMedia.addEventListener("change", handleChange)
+    }
+    return () => {
+      if (matchMedia.removeListener) {
+        matchMedia.removeListener(handleChange)
+      } else {
+        matchMedia.removeEventListener("change", handleChange)
+      }
+    }
+  }, [query])
+
+  return matches
+}
+
+export type { MediaQueryDirection, UseMediaQueryOptions }
+
