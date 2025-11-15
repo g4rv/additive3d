b47ee215abb59ab1624e685fa8b61be8b64e0aed
Serhii Volskyi <serhii.volskyi@radity.com>
2025-11-15 18:57:48 +0200

    change content flow in `footer`

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -12,58 +12,28 @@ export default function Footer() {
   return (
     <footer className="bg-base-100 border-base-300 border-t">
       <div className="custom-container pt-8">
-        <div className="grid grid-cols-1 gap-10 pb-6 sm:gap-6 lg:grid-cols-2">
-          <div>
-            <div className="text-base-content/80 mb-6 max-w-sm text-sm font-medium">
-              <Link href="/" aria-label="Additive3D - Головна" className="mb-2 inline-block w-fit">
-                <Image
-                  loading="lazy"
-                  src="/logo.png"
-                  alt="Additive3D Логотип"
-                  width={140}
-                  height={44}
-                />
-              </Link>
+        <div className="grid lg:grid-cols-2 grid-rows-[auto_1fr] gap-x-6 gap-y-6 lg:gap-y-0 pb-10">
+          <div className="text-base-content/80 lg:mb-6 max-w-sm text-sm font-medium">
+            <Link href="/" aria-label="Additive3D - Головна" className="mb-2 inline-block w-fit">
+              <Image
+                loading="lazy"
+                src="/logo.png"
+                alt="Additive3D Логотип"
+                width={140}
+                height={44}
+              />
+            </Link>
 
-              <p>Професійні послуги 3D-друку та сканування для бізнесу та приватних клієнтів.</p>
-            </div>
-
-            <div className="flex w-fit flex-col">
-              <p className="text-primary mb-1 font-semibold">Контакти</p>
-              <ul className="flex flex-col gap-1">
-                <li>
-                  <ButtonLink variant="subtle" href={CONTACT_INFO.email.link}>
-                    {CONTACT_INFO.email.label}
-                  </ButtonLink>
-                </li>
-                <li>
-                  <ButtonLink variant="subtle" href={CONTACT_INFO.phone.link}>
-                    {CONTACT_INFO.phone.label}
-                  </ButtonLink>
-                </li>
-                <li>
-                  <ButtonLink variant="subtle" href={CONTACT_INFO.phone2.link}>
-                    {CONTACT_INFO.phone2.label}
-                  </ButtonLink>
-                </li>
-                <li>
-                  <address className="not-italic">
-                    <ButtonLink variant="subtle" external href={CONTACT_INFO.address.link}>
-                      {CONTACT_INFO.address.label}
-                    </ButtonLink>
-                  </address>
-                </li>
-              </ul>
-            </div>
+            <p>Професійні послуги 3D-друку та сканування для бізнесу та приватних клієнтів.</p>
           </div>
 
-          <nav aria-label="Додаткова навігація" className="lg:mt-14">
+          <nav aria-label="Додаткова навігація" className="row-span-2 lg:mt-14">
             <ul className="grid grid-cols-2 gap-6 sm:grid-cols-3 lg:grid-cols-2 2xl:grid-cols-3">
               {MAIN_NAVIGATION_LIST.map((item) => (
                 <li key={item.href}>
                   <ButtonLink
                     variant="string"
-                    className="mb-1 text-base"
+                    className="mb-1 text-start text-base"
                     href={item.href}
                     active={pathname === item.href}
                   >
@@ -77,6 +47,7 @@ export default function Footer() {
                           <ButtonLink
                             variant="subtle"
                             href={child.href}
+                            className="text-start"
                             active={pathname === child.href}
                           >
                             {child.label}
@@ -89,6 +60,34 @@ export default function Footer() {
               ))}
             </ul>
           </nav>
+
+          <div className="col-end-2 flex w-fit flex-col">
+            <p className="text-primary mb-1 font-semibold">Контакти</p>
+            <ul className="flex flex-col gap-1">
+              <li>
+                <ButtonLink variant="subtle" href={CONTACT_INFO.email.link}>
+                  {CONTACT_INFO.email.label}
+                </ButtonLink>
+              </li>
+              <li>
+                <ButtonLink variant="subtle" href={CONTACT_INFO.phone.link}>
+                  {CONTACT_INFO.phone.label}
+                </ButtonLink>
+              </li>
+              <li>
+                <ButtonLink variant="subtle" href={CONTACT_INFO.phone2.link}>
+                  {CONTACT_INFO.phone2.label}
+                </ButtonLink>
+              </li>
+              <li>
+                <address className="not-italic">
+                  <ButtonLink variant="subtle" external href={CONTACT_INFO.address.link}>
+                    {CONTACT_INFO.address.label}
+                  </ButtonLink>
+                </address>
+              </li>
+            </ul>
+          </div>
         </div>
 
         <p className="text-base-content/50 border-base-300 border-t py-4 text-center text-sm">
